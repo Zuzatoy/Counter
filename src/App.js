cd03d2d89ae712ac096b4bd7233ca6a75711af5e
Unknown <zuzatoy@gmail.com>
2018-11-27 16:21:08 +1300

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    count: 0,
+    maxNumber: 10,
+    minNumber: 0,
+    message: ""
+  };
+
+  incrementCount = () => {
+    const { count, maxNumber } = this.state;
+    const newCount = count < maxNumber ? count + 1 : count;
+    const message = newCount === maxNumber ? "You win!" : "Keep going!";
+
+    this.setState({
+      count: newCount,
+      message
+    });
+  };
+
+  decrementCount = () => {
+    this.setState({
+      count: this.state.count - 1,
+      message: ""
+    });
+  };
+
+  resetCount = () => {
+    this.setState({
+      count: 0,
+      message: ""
+    });
+  };
+
   render() {
+    const { count, maxNumber, minNumber, message } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>{count}</h1>
+        <button disabled={count === maxNumber} onClick={this.incrementCount}>
+          +
+        </button>
+        <button
+          disabled={count === 0 || count === maxNumber}
+          onClick={count > minNumber ? this.decrementCount : null}
+        >
+          -
+        </button>
+        {count === maxNumber && (
+          <button onClick={this.resetCount}>Reset counter</button>
+        )}
+        <h2>{message}</h2>
       </div>
     );
   }

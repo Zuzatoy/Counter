8833e5cd18ec608e9eb77a7a6be222e813ed6c33
Unknown <zuzatoy@gmail.com>
2018-11-28 15:30:17 +1300

    rebuild

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-import Buttons from './Buttons'
+import { Message } from '../Message';
+import { Button } from '../Button';
+import { Counter } from '../Counter'
 
-export const App = () => {
-  return (
-    <Buttons/>
-  )
+export class App extends Component {
+  state = {
+    count: 0,
+    maxNumber: 10,
+    minNumber: 0,
+    message: ''
+  };
+
+  incrementCount = () => {
+    const { count, maxNumber } = this.state;
+    const newCount = count < maxNumber ? count + 1 : count;
+    const message = newCount === maxNumber ? "You win!" : "Keep going!";
+
+    this.setState({
+      count: newCount,
+      message
+    });
+  };
+
+  decrementCount = () => {
+    this.setState({
+      count: this.state.count - 1,
+      message: ""
+    });
+  };
+
+  resetCount = () => {
+    this.setState({
+      count: 0,
+      message: ""
+    });
+  };
+
+  render () {
+    const { count, maxNumber, minNumber, message } = this.state;
+
+    return (
+          <div>
+            <Button 
+              maxNum={maxNumber}
+              minNum={minNumber} 
+              actions={[this.decrementCount, 
+                        this.resetCount, 
+                        this.incrementCount]} 
+                      />
+            <Message
+              message={message}
+              />
+
+            <Counter
+              counter={count}
+              />
+          </div>
+
+    )
+  }
 }
-
-export default App
